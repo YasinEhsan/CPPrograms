368f662dc6349d36fed974a3c291c342d487905f
Yasin Ehsan <yasinehsan11@gmail.com>
2018-04-15 22:01:36 -0400

    Update Midterm.cpp

--- Midterm.cpp
+++ Midterm.cpp
@@ -233,4 +233,35 @@ int printSolution(int dist[], int n)
    for (int i = 0; i < V; i++)
       printf("%d tt %d\n", i, dist[i]);
 }
+// driver program to test above function
+int main()
+{
+   /* Let us create the example graph discussed above */
+   int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                      {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                      {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                      {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                      {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                      {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                      {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                      {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                      {0, 0, 2, 0, 0, 0, 6, 7, 0}
+                     };
+  
+    dijkstra(graph, 0);
+  
+    return 0;
+}
+//finds all divisors: discrete math 101 style
+#include<cmath>
+void printDivisors(int n) {
+    for (int i=1; i<=sqrt(n); i++){
+        if (n%i == 0){
+            if (n/i == i)
+                cout << i << " x " << i;
+            else 
+                cout << i << " x " << n/i << endl;
+        }
+    }
+}
 
