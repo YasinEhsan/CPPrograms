0cfde388ce10a3e7d08040562a5c7739f312eb90
Yasin Ehsan <yasinehsan11@gmail.com>
2018-04-18 15:40:10 -0400

    Update Hanoi.cpp

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -10,7 +10,7 @@ int main() {
    cin >> n;
    cout << endl;
    // The initial value of to depends on whether n is odd or even
-   int from = 0, to = _____, candidate = 1, move = 0;
+   int from = 0, to = from +1, candidate = 1, move = 0;
    
    // Initialize the towers
    for(int i = n + 1; i >= 1; --i)
@@ -19,27 +19,47 @@ int main() {
    t[2].push_back(n+1);
    
    while (t[1].size() < n+1) { // while t[1] does not contain all of the rings
-      cout << "Move #" << ++move << ": Transfer ring " << candidate << " from tower " << char(from+'A') << " to tower " << char(to+'A') << "\n";
+      cout << "Move #" << ++move << ": Transfer ring " << candidate << " from tower " << char(from +'A') << " to tower " << char(to +'A') << "\n"; // A cud also be rep. with 65
       
       // Move the ring from the "from tower" to the "to tower" (first copy it, then delete it from the "from tower")
-      _____
-      _____
+     t[to].push_back(t[from].back());
+     t[from].pop_back();
       
       // from = the index of the tower with the smallest ring that has not just been moved: (to+1)%3 or (to+2)%3
-      if (_____)
-         from = _____
+      if (t[(to + 1)%3].back() >  t[(to + 2)%3].back())
+         from = (to + 2)%3;
       else
-         from = _____
+         from = (to + 1)%3;
          
       // candidate = the ring on top of the t[from] tower
-      candidate = _____
+      candidate = t[from].back();
          
       // to = the index of the closest tower on which the candidate can be placed: (from+1)%3 or (from+2)%3
       // (compare the candidate with the ring on the closer tower; which tower is "closer" depends on whether n is odd or even)
-      if (_____)
-         to = _____
+      if (t[from].back() > t[(from + 1)%3].back() )
+         to = t[(from + 2)%3].back();
       else
-         to = _____
+         to = t[(from + 1)%3].back();
    }
    return 0;
 }
+
+//SAMPLE OUTPUT...
+
+// Please enter the number of rings to move: 4
+
+// Move #1: Transfer ring 1 from tower A to tower C
+// Move #2: Transfer ring 2 from tower A to tower B
+// Move #3: Transfer ring 1 from tower C to tower B
+// Move #4: Transfer ring 3 from tower A to tower C
+// Move #5: Transfer ring 1 from tower B to tower A
+// Move #6: Transfer ring 2 from tower B to tower C
+// Move #7: Transfer ring 1 from tower A to tower C
+// Move #8: Transfer ring 4 from tower A to tower B
+// Move #9: Transfer ring 1 from tower C to tower B
+// Move #10: Transfer ring 2 from tower C to tower A
+// Move #11: Transfer ring 1 from tower B to tower A
+// Move #12: Transfer ring 3 from tower C to tower B
+// Move #13: Transfer ring 1 from tower A to tower C
+// Move #14: Transfer ring 2 from tower A to tower B
+// Move #15: Transfer ring 1 from tower C to tower B
