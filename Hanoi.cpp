e4a4593280960ac2568b29b1a523bd0cee047a37
Yasin Ehsan <yasinehsan11@gmail.com>
2018-04-23 09:51:12 -0400

    Update Hanoi.cpp

--- Hanoi.cpp
+++ Hanoi.cpp
@@ -1,4 +1,10 @@
-//Yasin Ehsan
+//Yasin Ehsan NOTES...
+
+// - modulo 3 cuz 3 placeholders
+// - the buffer charcter cud be null
+// - char is 65 which is ascii from A
+// - candidate has to be "less" than the to.back
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -19,11 +25,15 @@ int main() {
    t[2].push_back(n+1);
    
    while (t[1].size() < n+1) { // while t[1] does not contain all of the rings
-      cout << "Move #" << ++move << ": Transfer ring " << candidate << " from tower " << char(from +'A') << " to tower " << char(to +'A') << "\n"; // A cud also be rep. with 65
+      cout << "Move #" << ++move << ": Transfer ring " << candidate << " from tower " << char(from +'A') << " to tower " << char(to +'A') << "\n"; 
+      
+      
       
       // Move the ring from the "from tower" to the "to tower" (first copy it, then delete it from the "from tower")
      t[to].push_back(t[from].back());
      t[from].pop_back();
+      
+      
       
       // from = the index of the tower with the smallest ring that has not just been moved: (to+1)%3 or (to+2)%3
       if (t[(to + 1)%3].back() >  t[(to + 2)%3].back())
@@ -31,15 +41,19 @@ int main() {
       else
          from = (to + 1)%3;
          
+         
+         
       // candidate = the ring on top of the t[from] tower
       candidate = t[from].back();
          
+         
+         
       // to = the index of the closest tower on which the candidate can be placed: (from+1)%3 or (from+2)%3
       // (compare the candidate with the ring on the closer tower; which tower is "closer" depends on whether n is odd or even)
-      if (t[from].back() > t[(from + 1)%3].back() )
-         to = t[(from + 2)%3].back();
+      if (t[(from + 1)%3].back() > candidate )
+         to = (from + 1)%3;
       else
-         to = t[(from + 1)%3].back();
+         to = (from + 2)%3;
    }
    return 0;
 }
