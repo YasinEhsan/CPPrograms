d035d72d9d1693848774262f55cbdcdd1c6b1919
Yasin Ehsan <yasinehsan11@gmail.com>
2018-05-09 09:56:01 -0400

    heavy debug Sucessful SP R

--- ShortestPath.cpp
+++ ShortestPath.cpp
@@ -1,37 +1,75 @@
+//Yasin Ehsan
+
 #include <iostream>
 #include <algorithm>
 #include <string>
 using namespace std;
 
-const int NUM_ROWS = 5, NUM_COLS = 6;
+const int NUM_ROWS = 5, NUM_COLS = 6; //'const' = final in java
 string path[NUM_ROWS][NUM_COLS];
 
-// Calculates the cost of every square and fills in the cost matrix.
-void calculateCosts() {
+// Returns the cost of the shortest path from the left to the square in row i, column j.
+int calculateCost(int i, int j) {
    static int weight[NUM_ROWS][NUM_COLS] = {{3,4,1,2,8,6},
                                             {6,1,8,2,7,4},
                                             {5,9,3,9,9,5},
                                             {8,4,1,3,2,6},
                                             {3,7,2,8,6,4}};
    // Declare the cost matrix.
-   _____
-   // Copy the leftmost column of the weight matrix to the cost matrix, and initialize the leftmost column of the path matrix.
-   _____
-   // For each remaining column,
-      // Calculate the cost of each square in the column (non-recursively),
-      // and store the correct number in the cost matrix and the correct string in the path matrix.
-   _____
+   static int cost[NUM_ROWS][NUM_COLS] = {0};
 
-   int minRow = 0;
-   // Check which square in the rightmost column has the lowest cost.  Store the row number of that square in minRow.
-   _____
-   
-   cout << "The length of the shortest path is " << cost[minRow][NUM_COLS-1];
-   cout << ".\nThe rows of the path from left to right are " << path[minRow][NUM_COLS-1] << ".";
+
+   // If the cost has already been calculated, return it.
+   if(cost[i][j] != 0)
+      return cost[i][j];
+
+
+   // Check for the base case.
+   if (j == 0) {
+      path[i][j] = to_string(i);
+      return weight[i][j];
+   }
+
+   // Calculate the costs of the 3 adjacent squares by calling the function recursively.
+   int up = calculateCost((i-1 + NUM_ROWS) % NUM_ROWS, j-1);
+   int left = calculateCost(i, j-1);
+   int down = calculateCost((i+1)%NUM_ROWS, j-1);
+
+   // Find the minimum of the 3 costs.
+   int minCost = min(min(up,down), left);
+
+
+   // Update the path matrix (store the path to the current square in path[i][j]):
+   // If up is the minimum, get the shortest path to the up-left square from the path matrix and concatenate it with the current row.
+    if(minCost==up)
+      path[i][j] = path[(i-1 + NUM_ROWS) % NUM_ROWS][j-1] + to_string(i);//
+    else if(minCost==down)
+      path[i][j] = path[(i+1)%NUM_ROWS][j-1] + to_string(i+1);
+    else
+      path[i][j] = path[i][j-1] + to_string(i);
+
+    cost[i][j] = weight[i][j] + minCost;
+
+    return cost[i][j];
+
+   // Calculate the cost of the current square, store the correct number in the cost matrix, and return the cost.
+
+
 }
 
 int main() {
-   calculateCosts();
+   int minRow = 0;
+   // Call the calculateCost function once for each square in the rightmost column of the grid.
+   // Check which one has the lowest cost and store the row number in minRow.
+
+
+  for(int i = 0; i < NUM_ROWS; i++){
+    int minCost = calculateCost(minRow, NUM_COLS-1);
+    if(minCost > calculateCost(i, NUM_COLS-1))
+        minRow = i;
+  }
+   cout << "The length of the shortest path is " << calculateCost(minRow, NUM_COLS-1);
+   cout << ".\nThe rows of the path from left to right are " << path[minRow][NUM_COLS-1] << ".";
 
    return 0;
 }
