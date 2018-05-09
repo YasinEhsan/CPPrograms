79b1bf4fc9aea0620539e6a9a049ea903207a397
Yasin Ehsan <yasinehsan11@gmail.com>
2018-05-09 09:53:58 -0400

    Update Midterm.cpp

--- Midterm.cpp
+++ Midterm.cpp
@@ -1,5 +1,23 @@
 #include<iostream>
 using namespace std;
+//post exam
+#include <iostream>
+using namespace std;
+
+void p(int);
+
+int main() {
+  p(0);
+}
+
+void p(int x){
+  if (x == 3)
+    return;
+  //cout << x << endl;
+  p(x+1);
+  cout << x << endl;
+}
+----------------------------------
 
 int backtracking() {
    int q[8] = {}; 
