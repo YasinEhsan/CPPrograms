21111ef7e62278ad33af58d6feb75bc15b3d6b9e
Yasin Ehsan <yasinehsan11@gmail.com>
2018-05-09 11:04:36 -0400

    added comments

--- ShortestPath.cpp
+++ ShortestPath.cpp
@@ -1,5 +1,6 @@
 //Yasin Ehsan
-
+//The modulo project. IN ccp -1%5 retuerns -1. Sup to b 4.
+// Fix: (-1 + 5)%5.
 #include <iostream>
 #include <algorithm>
 #include <string>
@@ -8,21 +9,19 @@ using namespace std;
 const int NUM_ROWS = 5, NUM_COLS = 6; //'const' = final in java
 string path[NUM_ROWS][NUM_COLS];
 
-// Returns the cost of the shortest path from the left to the square in row i, column j.
 int calculateCost(int i, int j) {
    static int weight[NUM_ROWS][NUM_COLS] = {{3,4,1,2,8,6},
                                             {6,1,8,2,7,4},
                                             {5,9,3,9,9,5},
                                             {8,4,1,3,2,6},
                                             {3,7,2,8,6,4}};
-   // Declare the cost matrix.
-   static int cost[NUM_ROWS][NUM_COLS] = {0};
+   // BAse case1
+   static int cost[NUM_ROWS][NUM_COLS]; //pre set to '0'
 
 
-   // If the cost has already been calculated, return it.
+   //Base case 2
    if(cost[i][j] != 0)
       return cost[i][j];
-
 
    // Check for the base case.
    if (j == 0) {
@@ -30,12 +29,9 @@ int calculateCost(int i, int j) {
       return weight[i][j];
    }
 
-   // Calculate the costs of the 3 adjacent squares by calling the function recursively.
-   int up = calculateCost((i-1 + NUM_ROWS) % NUM_ROWS, j-1);
+   int up = calculateCost((i-1 + NUM_ROWS) % NUM_ROWS, j-1); //*****MAJOR MOD PRBLM
    int left = calculateCost(i, j-1);
    int down = calculateCost((i+1)%NUM_ROWS, j-1);
-
-   // Find the minimum of the 3 costs.
    int minCost = min(min(up,down), left);
 
 
@@ -52,22 +48,15 @@ int calculateCost(int i, int j) {
 
     return cost[i][j];
 
-   // Calculate the cost of the current square, store the correct number in the cost matrix, and return the cost.
-
-
 }
 
 int main() {
    int minRow = 0;
-   // Call the calculateCost function once for each square in the rightmost column of the grid.
-   // Check which one has the lowest cost and store the row number in minRow.
 
-
-  for(int i = 0; i < NUM_ROWS; i++){
-    int minCost = calculateCost(minRow, NUM_COLS-1);
-    if(minCost > calculateCost(i, NUM_COLS-1))
+  for(int i = 0; i < NUM_ROWS; i++)
+    if(calculateCost(minRow, NUM_COLS-1) > calculateCost(i, NUM_COLS-1))
         minRow = i;
-  }
+
    cout << "The length of the shortest path is " << calculateCost(minRow, NUM_COLS-1);
    cout << ".\nThe rows of the path from left to right are " << path[minRow][NUM_COLS-1] << ".";
 
